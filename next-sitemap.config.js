e771e03458aab1d5775b388671ad5837e3136476
Ade-Adex <amoleadeoluadex@gmail.com>
2026-08-12 10:11:05 +0100

    feat: Created the Privacy policy  and Terms and condition page

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.rockandsafety.com',
+  generateRobotsTxt: true, // Automatically creates robots.txt as well
+  // (Optional) add paths to exclude from sitemap
+  // exclude: ['/admin/*'],
+}
